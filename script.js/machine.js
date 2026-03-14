55ef98d742642ab7e734ed789fa7f58aff08cbfe
TKR-503 <tawhidrafe2050@gmail.com>
2026-03-14 20:39:21 +0600

    Added the delete function

--- script.js/machine.js
+++ script.js/machine.js
@@ -5,13 +5,37 @@ function showTab(tab) {
 }
 
 function setInterview(id) {
-    const job = jobs.find(j => j.id === id);
+    const job = jobs.find(function(j) {
+        return j.id === id;
+     });
     job.status = "interview";
     renderJobs();
 }
 
 function setRejected(id) {
-    const job = jobs.find(j => j.id === id);
+    const job = jobs.find(function(j) { 
+        return j.id === id; 
+    });
     job.status = "rejected";
     renderJobs();
+}
+
+function deleteJob(id) {
+    const index = jobs.findIndex(function(j) { 
+        return j.id === id; 
+    });
+     if (currentTab === "all") {
+
+        const index = jobs.findIndex(function(j) { 
+            return j.id === id; 
+        });
+        jobs.splice(index, 1);
+
+    } else {
+         job.status = "all";
+
+
+}
+
+  renderJobs();
 }
